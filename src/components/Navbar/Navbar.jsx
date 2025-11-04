6c754e11702e292b13c2c9535573d19002a1ae9e
Amandaanderssn <tindraamanda@hotmail.se>
2025-11-04 13:37:57 +0100

    navbar is sticky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 
 import { House, BriefcaseBusiness, AtSign } from 'lucide-react';
 import LightDarkModeToggle from "../LightDarkModeToggle/LightDarkModeToggle";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+
     const iconSize = 20;
 
     const navitems = [
@@ -13,8 +18,29 @@ const Navbar = () => {
         { to: "/contact", icon: <AtSign size={iconSize} />, label: "Contact" },
     ];
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            // scrollar nedåt → göm navbar
+            if (currentScrollY > lastScrollY && currentScrollY > 700) {
+                setVisible(false);
+            }
+            // scrollar uppåt → visa navbar
+            else if (currentScrollY < lastScrollY) {
+                setVisible(true);
+            }
+
+            setLastScrollY(currentScrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [lastScrollY]);
+
     return (
-        <nav>
+        <nav className={visible ? "visible" : "hidden"}>
             <h1>Logo</h1>
             <div className="nav-links">
                 {navitems.map((item, index) => (
