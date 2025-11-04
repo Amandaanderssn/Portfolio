cbc7912015e6c13a769c2c5107efbd01154bb91b
Amandaanderssn <tindraamanda@hotmail.se>
2025-11-04 13:30:01 +0100

    Added some more minor animation to about and contact section

--- src/components/HomePageSections/AboutSection/AboutSection.jsx
+++ src/components/HomePageSections/AboutSection/AboutSection.jsx
@@ -1,22 +1,16 @@
+import { UseAnimationIfVisible } from "../../../hooks/UseAnimationIfVisible.jsx";
 import VerticalTitle from "../../VerticalTitle/VerticalTitle.jsx";
 import "./AboutSection.css";
-import { CircleChevronRight } from 'lucide-react';
 
 const AboutSection = () => {
 
+    const [ref, isVisible] = UseAnimationIfVisible(0.5)
+
     return (
-        <div className="about-section">
-            {/* <h2 className="title">About Me</h2> */}
+        <div className="about-section" ref={ref}>
             <VerticalTitle text="About Me" placement={"left"} />
             <div className="content">
-                <div className="position">
-                    {/* <div className="arrow">
-                        <CircleChevronRight />
-                    </div>
-                    <p className="about-text">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore molestiae facilis perferendis illo deserunt magni nobis ex voluptates voluptas temporibus sed nulla iusto cum fugit quo numquam doloremque ad similique, explicabo neque quod ea suscipit! Numquam, vero nam quas, ipsam quisquam molestiae corrupti quos unde dolorem ad impedit. Saepe illum at, obcaecati impedit molestias nam dolores? Incidunt reprehenderit, modi at illum dolorem quo voluptatem, voluptates sequi officiis nostrum laborum culpa rerum sed voluptatibus, maiores commodi nobis cupiditate voluptatum. Saepe possimus laborum soluta! Molestias et nihil, velit commodi beatae, doloremque eum, vitae necessitatibus dolore repellat officia provident! Quae soluta velit odio.
-                    </p> */}
-
+                <div className={`position ${isVisible ? "visible" : ""}`}>
                     <div>
                         <h3 style={{ textTransform: "uppercase" }}>Hi, I'm Amanda, I love making things that help people do their thing.</h3>
                     </div>
--- src/components/HomePageSections/ContactSection/ContactSection.jsx
+++ src/components/HomePageSections/ContactSection/ContactSection.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { UseAnimationIfVisible } from "../../../hooks/UseAnimationIfVisible";
 import "./ContactSection.css"
+import { useTheme } from "../../../hooks/UseTheme";
 
 const ContactSection = () => {
+    const { theme } = useTheme()
 
     const [ref, isVisible] = UseAnimationIfVisible(0.3)
     const [copied, setCopied] = useState(false);
@@ -15,13 +17,15 @@ const ContactSection = () => {
         setTimeout(() => setCopied(false), 500);
     }
 
+    const lightOrDarkButton = theme === "dark" ? "darkCopyButton" : "lightCopyButton"
+
     return (
         <div className="contact-section" ref={ref}>
             <h2 className={`connect-title ${isVisible ? 'visible' : ''} `}> Let's connect</h2>
             <div className={`mail ${isVisible ? 'visible' : ''} `}>
                 <p>{mail}</p>
                 {copied && <span className="copiedBubble">Copied!</span>}
-                <button className={`copyButton ${copied ? "clicked" : ""}`} onClick={copyMail}>CTRL + C </button>
+                <button className={`${lightOrDarkButton} ${copied ? "clicked" : ""}`} onClick={copyMail}>CTRL + C </button>
             </div>
             <p className={`thanksMsg ${isVisible ? 'visible' : ''} `}>Hope to hear from you</p>
         </div>
